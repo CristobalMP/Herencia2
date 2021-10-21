120d9cb9409931b08b18170acd6860bd3ecaea80
CristobalMP <msystemas@gmail.com>
2021-10-20 21:28:54 -0300

    Ejercicio

---
+++ Herencia2app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Herencia2app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PersonaApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
